385041e79c030d3647d44cf574d8bf875d078ffb
Jasonj03227 <jasonj03227@gmail.com>
2023-01-21 13:07:38 -0500

    Made utils folder, started aprilcam

---
+++ src/main/java/frc/robot/utils/AprilCam.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.utils;
+
+/** Add your docs here. */
+public class AprilCam {
+
+    private double distance;
+    private double horizontalOffset;
+
+    public AprilCam() {
+        distance = 0.00;
+        horizontalOffset = 0.00;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public double getHorizontalOffset() {
+        return horizontalOffset;
+    }
+
+
+}
