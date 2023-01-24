673b72f4d6d5bf73499235cb76daa5d09a22f07a
botkii <kennw902@gmail.com>
2023-01-24 18:18:41 -0500

    debugged arm code - KW

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,22 +1,33 @@
-    import edu.wpi.first.wpilibj2.command.SubsystemBase;
-    private DoubleSolenoid Army;
-    private static Arm instance;
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+   
     public class Arm extends SubsystemBase {
-public Arm(){
-      Army=(0, PneumaticsModuleType.REVPH, 1, 2);
-}
-     public static Arm getInstance() {
-    if (instance == null) {
-      instance = new Arm();
-    }
-    return instance;
-  }
-  public void armReach(){
-    Army.set(Value.kForward);
-  }
-  public void armRetract(){
-    Army.set(Value.kBackwards);
-  }
+      private DoubleSolenoid Army;
+      private static Arm instance;
+      
+      public Arm() {
+        Army= new DoubleSolenoid(0, PneumaticsModuleType.REVPH, 1, 2);
+      } 
+      
+      public static Arm getInstance() {
+        if (instance == null) {
+          instance = new Arm();
+        }
+        return instance;
+      }
+  
+      public void armReach(){
+        Army.set(Value.kForward);
+      }
+      
+      public void armRetract(){
+        Army.set(Value.kReverse);
+      }
+      
 @Override
   public void periodic() {
     // This method will be called once per scheduler run
