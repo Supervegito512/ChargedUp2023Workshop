a2ae4f116ee4c2c31be8148d0784a83e85897be8
botkii <kennw902@gmail.com>
2023-01-24 18:17:47 -0500

    debugged arm code

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "ChargedUp2023";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "fc3c62050c408c59f0962dcc1d377cfc4b15a02d";
+  public static final String GIT_DATE = "2023-01-14 12:55:24 EST";
+  public static final String GIT_BRANCH = "advantage-kit";
+  public static final String BUILD_DATE = "2023-01-14 13:04:19 EST";
+  public static final long BUILD_UNIX_TIME = 1673719459476L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
