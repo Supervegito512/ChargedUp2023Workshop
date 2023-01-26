aa50a20e1187e381daec8b43e216acb9c95b7ffb
Mariam1519 <121454566+maiam1519@users.noreply.github.com>
2023-01-26 01:08:08 +0000

    CodeForArm

---
+++ arm.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class arm{
+    public static void main(string[] args){
+
+
+public class Arm extends SubsystemBase {
+  
+  private CANSparkMax ArmMotor;
+  public static Arm instance;
+ 
+  public Arm() {
+    armMotor=new CANSparkMax(16, MotorType.kBrushless);
+    armMotor.setInverted(false);
+    armMotor.setIdleMode(IdleMode.kBrake);
+}
+
+public Arm getInstance() {
+  if (instance == null) {
+    instance = new Arm();
+    }
+    return instance;
+}
+public void reachout() {
+  armMotor.set(1);
+}
+public void retractin() {
+  armMotor.set(-1);
+    }
+public void stop() {
+  armMotor.set(0);
+}
+@Override
+  public void periodic() {
+ }
+}
+
+    }
+}
