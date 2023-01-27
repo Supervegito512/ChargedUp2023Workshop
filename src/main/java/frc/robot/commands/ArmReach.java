ce484555b87cc1c219d2f4ac142043b9f31d7d90
Mariam1519 <121454566+maiam1519@users.noreply.github.com>
2023-01-27 20:25:11 +0000

    ArmReach(new)

--- src/main/java/frc/robot/commands/ArmReach.java
+++ src/main/java/frc/robot/commands/ArmReach.java
@@ -7,7 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Arm;
 
-public class ArnReach extends CommandBase {
+public class ArmReach extends CommandBase {
   private Arm arm;
   /** Creates a new ArnReach. 
    * @return */
